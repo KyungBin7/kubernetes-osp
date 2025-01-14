fb47edb1e5c35dbf208e53b91989d1b0fd2c74c7
Kyungbin7 <jkb12377@naver.com>
2025-01-14 12:22:02 +0900

    edit files 3

--- 5_calico_controller.sh
+++ 5_calico_controller.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
----
-apiVersion: operator.tigera.io/v1
-kind: Installation
-metadata:
-  name: default
-spec:
-  calicoNetwork:
-    bgp: Enabled
-    hostPorts: Enabled
-    ipPools:
-      - blockSize: 26
-        cidr: 192.168.0.0/16
-        encapsulation: IPIPCrossSubnet
-        natOutgoing: Enabled
-        nodeSelector: all()
-  registry: quay.io
-EOF
 
 helm repo add projectcalico https://docs.tigera.io/calico/charts
 kubectl create namespace tigera-operator
@@ -24,4 +7,4 @@ helm install calico projectcalico/tigera-operator --version v3.27.5 --namespace 
 kubectl taint node node1.example.com node-role.kubernetes.io/control-plane:NoSchedule-
 
 kubectl apply -f calico-quay-crd.yaml
-kubectl -n calico-system get pod
+kubectl -n calico-system get pod -w
